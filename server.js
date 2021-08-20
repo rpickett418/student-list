dce91da5152e0bf84e086fb8922a3a1f19aeabbf
rpickett418 <86981398+rpickett418@users.noreply.github.com>
2021-08-20 02:30:54 -0600

    dones

--- server.js
+++ server.js
@@ -2,18 +2,18 @@ const express = require('express')
 const path = require('path')
 const Rollbar = require('rollbar')
 let rollbar = new Rollbar({
-    accessToken: '1e99830635c14048bf605dd9fc2bd2f8',
+    accessToken: 'e4b0f33507bd40adae7119cf5de989c9',
     captureUncaught: true,
     captureUnhandledRejections: true
-
 })
 
 const app = express()
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'))
+    rollbar.info('html file served sucessfully')
 })
 
-const port = process.env.PORT || 4444
+const port = process.env.PORT || 4444;
 // I had app.listen(4444 first)
 app.listen(port, () => console.log('Right Money'))
